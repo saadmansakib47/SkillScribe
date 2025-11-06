1b3fc53e8c0f36bbb6a369f5a25f964c24554768
unknown <saadmansakib@iut-dhaka.edu>
2025-11-06 10:18:46 +0600

    fix (instructor/dashboard) : solved layout problem

--- src/app/instructor/dashboard/page.tsx
+++ src/app/instructor/dashboard/page.tsx
@@ -20,16 +20,15 @@ import RangeSelector from "@/components/chart/instructorChart/rangeSelector";
 import StatCards from "@/components/ui/StatCards";
 import InstructorLayout from "@/app/instructor/instructorlayout";
 
-
 export default function InstructorDashboard() {
   const [selectedRange, setSelectedRange] = useState<"7d" | "14d" | "30d" | "90d">("7d");
-  const [selectedMetric, setSelectedMetricState] = useState<"Followers" | "Likes" | "Views" | "Watch Time">("Followers");
-
+  const [selectedMetric, setSelectedMetricState] = useState<
+    "Followers" | "Likes" | "Views" | "Watch Time"
+  >("Followers");
 
   return (
     <InstructorLayout>
       <div className="flex min-h-screen bg-[#F9FAFB]">
-
         {/* Main Content */}
         <main className="flex-1 p-8">
           {/* Top Bar */}
@@ -47,25 +46,22 @@ export default function InstructorDashboard() {
             setSelectedRange={setSelectedRange}
           />
 
-          {/* Stats Cards ( clickable) */}
+          {/* Stats Cards (clickable) */}
           <StatCards
             selectedMetric={selectedMetric}
             setSelectedMetric={setSelectedMetricState}
           />
 
-
-
           {/* Line Chart Section */}
-          <InstructorLineChart selectedMetric={selectedMetric} selectedRange={selectedRange} />
+          <InstructorLineChart
+            selectedMetric={selectedMetric}
+            selectedRange={selectedRange}
+          />
+
           {/* Bottom Section (Courses in Progress + Pie Chart side by side) */}
           <div className="grid grid-cols-3 gap-6">
             {/* Courses in Progress */}
-            <div className="grid grid-cols-3 gap-6">
-              <CoursesInProgress />
-              <InstructorPieChart />
-            </div>
-
-
+            <CoursesInProgress />
 
             {/* Earning Pie Chart */}
             <InstructorPieChart />
@@ -75,7 +71,6 @@ export default function InstructorDashboard() {
           <div className="col-span-2 bg-white rounded-lg shadow-sm p-4 mt-4">
             <CourseStatsTable />
           </div>
-
         </main>
       </div>
     </InstructorLayout>
